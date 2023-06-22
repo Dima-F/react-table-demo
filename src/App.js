60d4c4e73791cf73332710424adc86530fa2b5bc
Dima Fiialo <dafw@live.ru>
2023-06-23 00:23:01 +0300

    column hiding, sticky

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import PaginationTable from './components/PaginationTable';
+import StickyTable from './components/StickyTable';
 
 function App() {
   return (
-    <div className="App">
-      <PaginationTable />
+    <div>
+      <StickyTable />
     </div>
   );
 }
--- src/components/columns.js
+++ src/components/columns.js
@@ -1,5 +1,4 @@
 import { format } from 'date-fns';
-import ColumnFilter from './ColumnFilter';
 
 export const COLUMNS = [
     {
@@ -79,4 +78,52 @@ export const GROUPED_COLUMNS = [
             }
         ]
     }
+]
+
+export const STICKY_COLUMNS = [
+    {
+        Header: 'Id',
+        Footer: 'Id',
+        accessor: 'id',
+        disableFilters: true,
+        sticky: 'left'
+    },
+    {
+        Header: 'First Name',
+        Footer: 'First Name',
+        accessor: 'first_name',
+        sticky: 'left'
+    },
+    {
+        Header: 'Last Name',
+        Footer: 'Last Name',
+        accessor: 'last_name',
+        sticky: 'left'
+    },
+    {
+        Header: 'Date of Birth',
+        Footer: 'Date of Birth',
+        accessor: 'date_of_birth',
+        Cell: ({ value }) => format(new Date(value), 'dd/MM/yyyy')
+    },
+    {
+        Header: 'Country',
+        Footer: 'Country',
+        accessor: 'country'
+    },
+    {
+        Header: 'Phone',
+        Footer: 'Phone',
+        accessor: 'phone'
+    },
+    {
+        Header: 'Age',
+        Footer: 'Age',
+        accessor: 'age'
+    },
+    {
+        Header: 'Email',
+        Footer: 'Email',
+        accessor: 'email'
+    },
 ]
